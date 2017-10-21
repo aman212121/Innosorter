aba8f5a5f3850026b2e1eb0b6e4ba47c6b4873d0
Aman <guptaaman2121@gmail.com>
2017-10-22 11:47:16 +1300

    Pass RegistrationServiceDaoTest

--- src/main/java/com/innohub/innosorter/authentication/RegistrationServiceDao.java
+++ src/main/java/com/innohub/innosorter/authentication/RegistrationServiceDao.java
@@ -12,10 +12,39 @@ public class RegistrationServiceDao {
 	public static String badPasswordMessage = "Weak Password";
 
 	public String registerUser(String username, String password) {
-		
+		int minLength =8;
+	    int digit =0;
+	    int upCharCount =0;
+	    int lowerCharCount =0;
+	    
 		if(systemUsers.containsKey(username)){
 			throw new RuntimeException( usernameAlreadyExistsMessage);
 		}
-		return successMessage;
+		
+		if(password.length() >= minLength){
+			
+			for(int i = 0; i < password.length(); i++){
+				char a = password.charAt(i);
+				//if uppercase character found increase count
+				if(Character.isUpperCase(a)){
+					upCharCount++;
+				}
+				//if lowercase character found increase count
+				if(Character.isLowerCase(a)){
+					lowerCharCount++;
+				}
+				//if digit found increase count
+				if(Character.isDigit(a)){
+					digit++;
+				}
+			}
+			//if all the conditions are met, add user
+			if (digit > 0 && upCharCount >0 && lowerCharCount >0){
+				systemUsers.put(username, password);
+				return successMessage;
+			}
+		}
+		
+		throw new RuntimeException( badPasswordMessage);
 	}
 }
