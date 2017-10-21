448d396b2e39d36ec65aa7321498f04975c5291a
Karthik Sriram <31844757+karthiksriramuoa@users.noreply.github.com>
2017-10-22 11:42:53 +1300

    Added a test
    Store Issue

---
+++ src/test/java/com/innohub/innosorter/repo/IssueRepositoryServiceTest.java
@@ -0,0 +1,55 @@
+package com.innohub.innosorter.repo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.AssertionFailedError;
+
+public class IssueRepositoryServiceTest implements IssueRepositoryService {
+
+	private IssueRepositoryService issueRepository;
+	@Before
+	void SetUp()
+	{
+		issueRepository = new IssueRepositoryService();
+	}
+	
+	@Test
+	public void shouldNotStoreIssuesWithoutContext() {
+		issueRepository = new IssueRepositoryService();
+		
+		Cluster issue = new Cluster();
+		issue.setName("IssueName1");
+		issue.setSummary("Javascript Not Working");
+		issue.setNoRelatedPosts("12");
+		issue.setNoOfUsers("20");
+		issue.setContext("");
+		issue.SetAssignees("Karthik");
+		issue.CurrentStatus("Assigned");
+	    issueRepository.storeIssue(issue);
+	    Boolean ifStoreIssuesWithoutContext = false;
+	    if(issue.) {
+	    	ifStoreIssuesWithoutContext = true;
+	    }
+	    assertTrue(ifStoreIssuesWithoutContext);
+	}
+	
+	@Test
+	public void shouldNotStoreIssuesWithoutUsersImpacted()
+	{
+		Cluster issue = new Cluster();
+		issue.setUsersImpacted("");
+		issue.set
+
+	}
+	
+	@Test
+	public void shouldFindIssueWithIssueTitle()
+	{
+		Issue issue = new Issue();
+		issueRepository.getTitle(issue);
+		
+	}
+
+}
