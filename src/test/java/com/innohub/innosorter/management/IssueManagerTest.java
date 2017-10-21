055725158a57495ee94df27413978fa36e82eacf
Hesam Emadi <hesam.emadi@triplejump.co.nz>
2017-10-21 19:20:08 +1300

    Added test case (green) : shouldNotAllowNonAdminUsersToAddAForumPostToACluster

--- src/test/java/com/innohub/innosorter/management/IssueManagerTest.java
+++ src/test/java/com/innohub/innosorter/management/IssueManagerTest.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.*;
 
 import java.util.List;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.innohub.innosorter.entity.Cluster;
 import com.innohub.innosorter.entity.Post;
@@ -29,5 +31,20 @@ public class IssueManagerTest {
 
         assertTrue(isPostExists);
     }
+    @Rule
+    public ExpectedException expected = ExpectedException.none();
     
+    @Test
+    public void shouldNotAllowNonAdminUsersToAddAForumPostToACluster(){
+//        Admin admin = new Admin("AdminName");
+
+        Cluster clusterOne = new Cluster();
+        Post postOne = new Post();
+
+        expected.expect(RuntimeException.class);
+        expected.expectMessage("User does not have enough priviledge to do this action.");
+
+        Developer developer = new Developer("DeveloperOne");
+        issueManager.addPostToCluser(clusterOne, postOne);
+    }
 }
