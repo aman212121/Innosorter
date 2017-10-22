418288b1ced8dd8cbde877bf28b51c4176eab0b9
Karthik Sriram <31844757+karthiksriramuoa@users.noreply.github.com>
2017-10-22 13:54:29 +1300

    Passed tests for store-issue-detail

--- src/main/java/com/innohub/innosorter/repo/IssueRepositoryServiceImpl.java
+++ src/main/java/com/innohub/innosorter/repo/IssueRepositoryServiceImpl.java
@@ -2,17 +2,17 @@ package com.innohub.innosorter.repo;
 
 import com.innohub.innosorter.entity.Cluster;
 import com.innohub.innosorter.repo.IssueRepositoryService;
+import com.innohub.innosorter.util.ApplicationConstants;
 
-public class IssueRepositoryServiceImpl implements IssueRepositoryService{
+public class IssueRepositoryServiceImpl implements IssueRepositoryService {
 
 	public void storeIssue(Cluster cluster) {
-		// TODO Auto-generated method stub
-		
+		if (cluster.getContext() == null) {
+			throw new RuntimeException(ApplicationConstants.CLUSTER_CONTEXT_NOT_AVAILABLE_MSG);
+		} else if (cluster.getTitle() == null) {
+			throw new RuntimeException(ApplicationConstants.CLUSTER_ISSUE_TITLE_NOT_AVAILABLE_MSG);
+		} else if (cluster.getNumOfUserImpacted() == null) {
+			throw new RuntimeException(ApplicationConstants.CLUSTER_NUM_OF_IMPACTED_USER_NOT_AVAILABLE_MSG);
+		}
 	}
-
-	public String getTitle(Cluster issue) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
