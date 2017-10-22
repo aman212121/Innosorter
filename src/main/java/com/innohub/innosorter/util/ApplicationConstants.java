5c06d24b1fe5b97e02b6ba7f5b1331179758d966
Karthik Sriram <31844757+karthiksriramuoa@users.noreply.github.com>
2017-10-22 13:03:07 +1300

    Resolved compile errors for feature store issue details;

--- src/main/java/com/innohub/innosorter/util/ApplicationConstants.java
+++ src/main/java/com/innohub/innosorter/util/ApplicationConstants.java
@@ -3,4 +3,11 @@ package com.innohub.innosorter.util;
 public class ApplicationConstants {
 
     public final static String DOES_NOT_PRIVILEGE_MSG = "User does not have enough priviledge to do this action.";
+
+    public final static String CLUSTER_CONTEXT_NOT_AVAILABLE_MSG = "Cluster does not have context.";
+    public final static String CLUSTER_NUM_OF_IMPACTED_USER_NOT_AVAILABLE_MSG = "Cluster does not have num of user impacted.";
+    public final static String CLUSTER_ISSUE_TITLE_NOT_AVAILABLE_MSG = "Cluster does not have issue title.";
+
+
+
 }
