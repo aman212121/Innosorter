48b368bf25f4786c891cfce603e35fbbd29d3813
Hesam Emadi <hesam.emadi@triplejump.co.nz>
2017-10-22 17:53:57 +1300

    Added test case: shouldAllowAdminUserToAddForumPostToCluster, added Mockit to pom file. Compile problem (red status). WIP

--- src/test/java/com/innohub/innosorter/management/IssueManagerTest.java
+++ src/test/java/com/innohub/innosorter/management/IssueManagerTest.java
@@ -1,28 +1,49 @@
 package com.innohub.innosorter.management;
 
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import org.mockito.Mock;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import com.innohub.innosorter.entity.Administrator;
 import com.innohub.innosorter.entity.Cluster;
 import com.innohub.innosorter.entity.Developer;
 import com.innohub.innosorter.entity.Post;
+import com.innohub.innosorter.repo.IssueRepositoryService;
 import com.innohub.innosorter.util.ApplicationConstants;
 
 public class IssueManagerTest {
 
     private IssueManager issueManager;
 
+    @Mock
+    private IssueRepositoryService issueRepositoryService;
+
     @Before
     public void setUp(){
+        initMocks(this);
         this.issueManager = new IssueManager();
     }
 
     @Rule
     public ExpectedException expected = ExpectedException.none();
-    
+
+    @Test
+    public void shouldAllowAdminUserToAddForumPostToCluster(){
+        Cluster clusterOne = new Cluster();
+        Post postOne = new Post();
+
+        Administrator admin = new Administrator("AdminUser");
+
+        issueManager.addPostToCluser(admin, clusterOne, postOne);
+
+//        verify()
+    }
+
     @Test
     public void shouldNotAllowNonAdminUsersToAddAForumPostToACluster(){
 
